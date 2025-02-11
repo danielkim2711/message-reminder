f15db965216794c104c3dcce9117e62d591d1507
Daniel Kim <danielkim2711@gmail.com>
2025-02-11 23:17:15 +1300

    feat: add message reminder

---
+++ reminder.py
@@ -0,0 +1,37 @@
+import time
+import pyautogui
+import schedule
+import pandas as pd
+from datetime import datetime
+import pyperclip
+
+def get_today_message():
+    df = pd.read_csv('messages.csv')
+    today = datetime.today().strftime('%Y-%m-%d')
+    message_row = df[df['date'] == today]
+    return message_row['message'].values[0] if not message_row.empty else '오늘의 메시지가 없습니다.'
+
+def send_message():
+    message = get_today_message()
+    pyperclip.copy(message)
+
+    pyautogui.click(x=862, y=344)
+    pyautogui.press('enter')
+
+    time.sleep(5)
+
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('v')
+    pyautogui.keyUp('v')
+    pyautogui.keyUp('command')
+
+    print(message)
+    pyautogui.press('enter')
+    print('Done!')
+
+# schedule.every().day.at('07:00').do(send_message)
+schedule.every(1).minutes.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
